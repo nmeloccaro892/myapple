4e79c985ad9eb599fc3d8a5768495802ff72866c
unknown <nmeloccaro892@wit-xdvm-082.MILLARDPS.NET>
2012-11-16 11:09:25 -0600

    Added Images folder

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="icon" type="image/png" href="img/apple-icon.png" />
+
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Scada:400italic,400,700"/>
+
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+		
+		<title>My Apple Products</title>
+	</head>
+	<body>
+		<div class="container">
+			<header>
+				<?php include('layout/header.php'); ?>
+			</header>
+			
+			<nav>
+				<?php include('layout/nav.php'); ?>
+			</nav>
+			
+			<div id="content">
+				<?php include('layout/content.php'); ?>
+			</div>
+			
+			<footer>
+				<?php include('layout/footer.php'); ?>
+			</footer>
+		</div>
+	</body>
+</html>
--- layout/nav.php
+++ layout/nav.php
@@ -1,1 +1,11 @@
-<?php
+<div class="navbar">
+	<div class="navbar-inner">
+		<div class="container">
+			<a href="./" class="brand"><img src="img/apple-icon.png" alt="" /></a>
+			<ul class="nav" role="navigation">
+				<li><a href="./?p=contacts">My Products</a></li>
+				<li><a href="./?p=form_add_contact">Add</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
